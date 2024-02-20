031ae6f080b14b33a237ddd961d13816a132616c
Kial Ayoub <132392508+Kialayoub@users.noreply.github.com>
2024-02-20 22:04:04 +0100

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 '''
-class definition of a State
+State class
 '''
-
-
 from sqlalchemy import Column, Integer, String, MetaData
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
-
 class State(Base):
-    ''' State Class '''
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
