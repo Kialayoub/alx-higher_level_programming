16818c6f0f51108d2c272d78fe3d9a932bf27cfa
Kial Ayoub <132392508+Kialayoub@users.noreply.github.com>
2024-02-20 21:43:59 +0100

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-script that lists all cities from the database
+ists all cities from the db
 '''
 
 import MySQLdb
@@ -8,17 +8,14 @@ import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(
+        port=3306,
+        host='localhost',
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306,
-        host='localhost')
+        db=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute(
-        'SELECT cities.id, cities.name, states.name FROM cities JOIN states ON\
-        cities.state_id = states.id;')
-
+    cursorexecute("""SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id=cities.state_id""")
     states = cursor.fetchall()
 
     for state in states:
