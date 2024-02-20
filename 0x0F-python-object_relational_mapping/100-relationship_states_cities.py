a91ac866be9642906c93c00ac727db520e3e3e9a
Kial Ayoub <132392508+Kialayoub@users.noreply.github.com>
2024-02-20 22:04:59 +0100

    Update 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
 '''
-creates the State “California” with the City “San Francisco”
+creates the “California” State with the  “San Francisco” City
 '''
-
-
 from sys import argv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
-
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
-    engine = create_engine(
+    db_engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
-    InstanceSession = sessionmaker(bind=engine)
-    session = InstanceSession()
+    Base.metadata.create_all(db_engine)
+    SessionMaker = sessionmaker(bind=db_engine)
+    db_session = SessionMaker()
 
-    new_state = State(name='California')
-    new_city = City(name='San Francisco', state=new_state)
-    session.add(new_city)
-    session.commit()
-
-    session.close()
+    new_state_obj = State(name='California')
+    new_city_obj = City(name='San Francisco',
