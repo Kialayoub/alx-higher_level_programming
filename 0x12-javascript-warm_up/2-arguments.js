1fc0c97b56f43ea47e85ab71ebfb5df043e96df8
Kialayoub <kial.ayoub@gmail.com>
2024-02-29 14:10:21 +0000

    correct some errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-// Displays a message based on the number of arguments provided
+// Prints a message depending of the number of arguments passed
 
 if (process.argv.length === 2) {
-  console.log('No arguments');
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-  console.log('Single argument found');
+  console.log('Argument found');
 } else {
-  console.log('Multiple arguments found');
+  console.log('Arguments found');
 }
-
