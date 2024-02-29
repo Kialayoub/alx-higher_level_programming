db2411af4f12a46a46fd64f604e571b88f9b2e25
Kial Ayoub <132392508+Kialayoub@users.noreply.github.com>
2024-02-29 15:12:22 +0100

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // Outputs two arguments passed to the script in the format: "argument1 is argument2"
 
-ole.log(process.argv[2] + ' is ' + process.argv[3]);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
 
